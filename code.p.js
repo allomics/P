19c682de9f7a98a405d014d93ae2a8474676f5e0
Prabhat Kumar <31214933+allomics@users.noreply.github.com>
2017-08-31 18:01:35 +0530

    committed.
    by Prabhat Kumar.

---
+++ code.p.js
@@ -0,0 +1,26 @@
+// a scope level syntax of P[Ψ].
+// =============================
+
+Ψ.actional(){
+
+Ψ.additional(){
+   Ψ.fn($a,$b,$c,$d){};
+   return;
+};
+
+Ψ.conditional(){
+   Ψ.fn($a,$b,$c,$d){};
+   return;
+};
+
+Ψ.traditional(){
+   Ψ.fn($a,$b,$c,$d){};
+   return;
+};
+
+Ψ.functional(){
+   Ψ.fn($a,$b,$c,$d){};
+   return;
+};
+
+}[];
